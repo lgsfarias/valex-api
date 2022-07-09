8ba182c6bb44a0a11b2cd4fd2243973541f26487
Luiz Gustavo Farias <lgsfarias.dev@gmail.com>
2022-07-08 22:55:30 -0300

    feat: AppError

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,8 +1,20 @@
 import { ErrorRequestHandler } from 'express';
+import AppError from '../utils/errors/AppError.js';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.log(err);
-  res.status(500).send('internal server error');
+  if (err instanceof AppError) {
+    console.log(err);
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 };
 
 export default errorHandler;
